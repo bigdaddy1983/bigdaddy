4c7f0651cc82b9e4cba97ff9c1b3ae2a979be9aa
bigdaddy1983 <lianyinglei@126.com>
2017-09-21 13:39:05 +0800

    aa

--- src/main/java/com/bigdaddy/bigdaddyCloud/controller/Person.java
+++ src/main/java/com/bigdaddy/bigdaddyCloud/controller/Person.java
@@ -2,25 +2,29 @@ package com.bigdaddy.bigdaddyCloud.controller;
 
 import java.io.Serializable;
 
-public class Person{
+public class Person {
 
 	/**
 	 * 年龄
 	 */
 	private Integer age;
 	private String name;
+
 	public Integer getAge() {
+		age++;
 		return age;
 	}
+
 	public void setAge(Integer age) {
 		this.age = age;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+
 }
